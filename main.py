b28ed2043790152991e8d1d7351b63e420f6d75d
250930 <trabajozorro@gmail.com>
2026-03-05 16:21:55 -0500

    error

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main():
             eliminar(empanadas)
 
         elif opcion == "5":
-            print("Programa finalizado")
+            print("Programa terminado")
             break
 
         else:
