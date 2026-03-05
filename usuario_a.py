10ed3e3e438167e0a793e29761ada097d745ee33
Samuellesmes45ex <samuellesmes45@gmail.com>
2026-03-05 15:53:57 -0500

    feat(usuario_a): Se crea funcion para listar

--- usuario_a.py
+++ usuario_a.py
@@ -12,3 +12,16 @@ def cargar_empanadas():
 def guardar_empanadas(empanadas):
     with open(archivo, "w") as f:
         json.dump(empanadas, f, indent=4)
+
+def listar(empanadas):
+
+    if len(empanadas) == 0:
+        print("No hay empanadas registradas")
+        return
+
+    for e in empanadas:
+        print("\n------------------")
+        print("Nombre:", e["nombre"])
+        print("Precio:", e["precio"])
+        print("Ingredientes:", e["ingredientes"])
+        print("Disponible:", e["disponible"])
