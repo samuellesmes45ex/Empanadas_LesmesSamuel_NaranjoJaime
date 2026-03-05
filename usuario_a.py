9f3aa054c36349fd9a913ed42750078973072f57
Samuellesmes45ex <samuellesmes45@gmail.com>
2026-03-05 15:48:21 -0500

    feat(usuario_b):Se crea la funcion para guardar en el json

--- usuario_a.py
+++ usuario_a.py
@@ -8,3 +8,7 @@ def cargar_empanadas():
             return json.load(f)
     except:
         return []
+
+def guardar_empanadas(empanadas):
+    with open(archivo, "w") as f:
+        json.dump(empanadas, f, indent=4)
